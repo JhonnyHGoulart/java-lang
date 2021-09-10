9d219167d5aaef4fd0b50a00993d7c5d734becdf
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-09-10 09:16:53 -0300

    Todos as classes implementam a classe Object

---
+++ src/br/com/senai/javalang/test/ObjectTest.java
@@ -0,0 +1,24 @@
+package br.com.senai.javalang.test;
+
+
+public class ObjectTest {
+    public static void main(String[] args) {
+        System.out.println("x");
+        System.out.println(3);
+        System.out.println(true);
+        println();
+    }
+
+    public static void println() {
+    }
+
+    public static void println(int valor) {
+    }
+
+    public static void println(boolean opcao) {
+    }
+
+    public static void println(Object referencia) {
+    }
+
+}
